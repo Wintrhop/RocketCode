83681d6af7c0df6aa112904a009ee3e2594b1877
jhonV <jhonv.v1@gmail.com>
2023-05-03 23:10:28 -0500

    ajuste de query en typedefs y resolver

--- src/models/typeDefs.ts
+++ src/models/typeDefs.ts
@@ -2,19 +2,24 @@ import gql from "graphql-tag";
 
 export const typeDefs = gql`
   type Query {
-    greetings: String
-    welcome(name: String!): String
+    Users: [User]
+    User(id: ID!): User!
   }
   #User Model Object
   type User {
-  id:ID
-  name:String
-  projectValue:Int
-  projectDepartment:String
-  city:String
+    id: ID
+    name: String
+    projectValue: Int
+    projectDepartment: String
+    city: String
   }
-  #User Mutation
+  #UserMutation
   type Mutation {
-    create(name: String, projectValue: Int, projectDepartment: String, city): User
+    create(
+      name: String
+      projectValue: Int
+      projectDepartment: String
+      city: String
+    ): User
   }
 `;
--- src/resolvers.ts
+++ src/resolvers.ts
@@ -1,6 +1,24 @@
+import User, { IUser } from "./models/User";
 export const resolvers = {
-    Query: {
-      greetings: () => "GraphQL is Awesome",
-      welcome: (parent:any, args:any) => `Hello ${args.name}`,
+  Query: {
+    Users: async (): Promise<IUser[]> => await User.find({}),
+    User: async (_: any, args: any): Promise<IUser> => {
+        const user = await User.findById(args.id)
+        return user!
+    
+    }
+  },
+  Mutation: {
+    create: async (_: any, args: any): Promise<IUser> => {
+      const { name, projectValue, projectDepartment, city } = args;
+      const user = {
+        name,
+        projectValue,
+        projectDepartment,
+        city,
+      };
+      const newUser = await User.create(user);
+      return newUser;
     },
-  };
+  },
+};
