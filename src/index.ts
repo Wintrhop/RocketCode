80d3f991e8d5d45876d1cbee3b4e456447b1b332
jhonV <jhonv.v1@gmail.com>
2023-05-04 01:54:46 -0500

    express middleware

--- src/index.ts
+++ src/index.ts
@@ -1,29 +1,50 @@
-
 import * as dotenv from "dotenv";
 dotenv.config();
 import { ApolloServer } from "@apollo/server";
-import { startStandaloneServer } from "@apollo/server/standalone";
-import { resolvers } from "./resolvers.js";
-import { typeDefs } from "./models/typeDefs.js";
+import { expressMiddleware } from "@apollo/server/express4";
+import { ApolloServerPluginDrainHttpServer } from "@apollo/server/plugin/drainHttpServer";
+import express from "express";
+import http from "http";
+import cors from "cors";
+import bodyParser from "body-parser";
+import { resolvers } from "./resolvers";
+import { typeDefs } from "./models/typeDefs";
 import mongoose from "mongoose";
 
-const server = new ApolloServer({ typeDefs, resolvers });
+const app = express();
+const httpServer = http.createServer(app);
 
 const OPTIONS = {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  };
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+};
 
 mongoose
   .connect(process.env.MONGO_URI as string)
   .then(() => {
     console.log(`connection with mongo OK`);
   })
-  .catch(err => {
+  .catch((err) => {
     console.log(err.message);
   });
-startStandaloneServer(server, {
-  listen: { port: Number(process.env.PORT) || 4000},
-}).then(() => {
-  console.log(`Server ready`);
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
 });
+
+(async () => {
+  await server.start();
+  app.use(
+    "/",
+    cors<cors.CorsRequest>(),
+    bodyParser.json({ limit: "50mb" }),
+    expressMiddleware(server, {
+      context: async ({ req }) => ({ token: req.headers.token }),
+    })
+  );
+  await new Promise<void>((resolve) =>
+    httpServer.listen({ port: Number(process.env.PORT) || 4000 }, resolve)
+  );
+  console.log(`Server ready`);
+})();
